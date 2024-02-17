ddd3168e650678e77a2bdf08450ea932efc7ce58
sbechata <97541449+sbechata@users.noreply.github.com>
2024-02-17 22:16:17 +0100

    Create project1.py

---
+++ project1.py
@@ -0,0 +1,11 @@
+## Project 1 : Band name generator
+
+# presentation message
+print("Welcome to the Band Name Generator.")
+
+# ask user for city + pet
+city = input("What's name of the city you grew up in? ")
+pet = input("What's your pet's name? ")
+
+# concatenation of city+pet 
+print("Your band name could be "+city+" "+ pet)
